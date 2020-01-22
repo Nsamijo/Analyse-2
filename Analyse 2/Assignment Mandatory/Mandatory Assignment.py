9af67c87aa4f058e6d45530bc95616fd84758ecd
Nathan Samijo <0961962@hr.nl>
2020-01-22 22:20:30 +0100

    Updated the Mandatory Assignment

--- Analyse 2/Assignment Mandatory/Mandatory Assignment.py
+++ Analyse 2/Assignment Mandatory/Mandatory Assignment.py
@@ -1,150 +1,132 @@
 import random
 import matplotlib.pyplot as plot
-import numpy as np
 
-#amount of players
-players = ['Strategy ' + str(x) for x in range(1, 4)]
-#amount of turns
-turns = [0, 0 ,0]
-#amount of rolls
-rolls = [0, 0, 0]
-#points from all the players
+#wins from strategies
+strategies = [0, 0, 0]
+
+#points from the strategies
 points = [0, 0, 0]
-#player turn
+
+#turn || which strategy should happen
 turn = 0
-#for how many we have rolled
+
+#rolled amount
 rolled = 0
-#turns for strategy 3 and checkpoint setting
-checkPoint = 0
-setPoint = True
 
-#total wins
-wins = [0, 0, 0]
-win = 0
+#wins ending
+MAX_WINS = 20
 
-def nextPlayer():
-    '''go to the next player'''
-    global turn, setPoint, rolled
-    #increment the turn
-    turn += 1
-    #if we surpassed the number of strategies turn gets reset to 0 || use of 2 because of list index
+#max points limit
+MAX_POINTS = 50
+
+#dice rolling
+def dice():
+    #roll the dice and return this
+    return random.randint(1, 6)
+
+#strategies from the players
+def strateGies(player):
+    global turn, rolled, points
+    #all strategies go here
+    if player == 0:
+        #player 1 || go till 12
+        if rolled >= 12:
+            #add the points to the total
+            points[0] += rolled
+            #reset the rolled
+            rolled = 0
+            #next player
+            turn += 1
+    elif player == 1:
+        #player 2 || go till 33
+        if rolled >= 33:
+            #add the points to the total
+            points[1] += rolled
+            #reset the points
+            rolled = 0
+            #next player
+            turn += 1
+    elif player == 2:
+        #player 3 || your strategy
+        turn += 1
+        pass
+
+    #limit the turn to the amount of players
     if turn > 2:
-        #reset turn
+        #reset the turn to the first player
         turn = 0
-    #reset rolled
-    rolled = 0
-    #reset the setpoint
-    setPoint = True
 
-def playerStrategy():
-    '''here we have the strategies of the players'''
-    global turn, turns, rolls, rolled, points
-    #check which limit should we abide to
-    if rolled >= [12, 33][turn]:
-        #add what is rolled to the totel of rolls and turns
-        rolls[turn] += rolled
-        points[turn] += rolled
-        #reset whar is rolled so that the next player does not inherit it
-        rolled = 0
-        #next player
-        nextPlayer()
+#pig game
+def piggy():
+    global rolled, turn
+    #roll the dice
+    temp = dice()
 
-def strategy3():
-    '''strategy 3 || OP'''
-    global checkPoint, turns, setPoint
-    global rolls, rolled, points
-    #save the point before out turn once
-    if setPoint:
-        #set a checkpoint
-        checkPoint = turns[2]
-        #set it False so we only do this once
-        setPoint = False
-
-    #check if we have rolled a number of times
-    if turns[2] - checkPoint == 6:
-        #add what is rolled to the total points and rolls
-        rolls[2] += rolled
-        points[2] += rolled
-        #next player
-        nextPlayer()
-
-def greedyPig():
-    '''this is the function for greedy pig'''
-    global rolled, turns, turn, rolls
-
-    def roll():
-        '''return a number'''
-        return random.randint(1, 6)
-
-    #roll and check what we have and increment turns
-    temp = roll()
-    #increment turns to know how many times we rolled
-    turns[turn] += 1
+    #end the players turn and lose all the points
     if temp == 1:
-        #reset the rolled
+        #player loses all points
         rolled = 0
         #next player
-        nextPlayer()
-    else:
-        #increment the rolled variable so the total rolled is saved
+        turn += 1
+    #if not a 1 (one) is rolled go to the strategy
+    elif temp > 1:
+        #add to the rolled, so this does not get lost
         rolled += temp
+        #go to the players strategy
+        strateGies(turn)
 
-        #check if strategy 1 or 2 are to go
-        if turn < 2:
-            #infidels strategies 
-            playerStrategy()
-        else:
-            #time for our strategy to shine
-            strategy3()
+#here the program will do the computing
+def research():
+    #get the max variables
+    global MAX_POINTS, MAX_WINS
+    #get the points, rolled, turn and strategies
+    global strategies, rolled, points
 
-def check():
-    '''check for the ending points'''
-    global points, wins, win
-    #check if we have a winner
-    for x in points:
-        #check if a strategy has surpassed the limit of points
-        if x >= 50:
-            #increment the points for the winning strategy
-            wins[points.index(x)] += 1
-            #return False to break out of the while loop
-            return False
-    return True
+    #keep running the greedy pig till the max wins are achieved
+    while sum(strategies) != MAX_WINS:
+        
+        #while the biggest is smaller then the maximum points to win 1 game, keep the game running
+        while not max(points) > MAX_POINTS:
+            #run the pig game once
+            piggy()
 
-#run till we reach maximum wins
-while sum(wins) != 100:
-    #reset the points
-    points = [0, 0, 0]
+        #dubbel check if is valid || is maybe not needed but you can never be sure
+        if max(points) > MAX_POINTS:
+            #get the winning strategy
+            player = points.index( max(points) )
+            #increment the winning strategy with 1
+            strategies[player] += 1
+
+            #reset the value to rerun the pig and stop bad data
+            points = [0, 0, 0]
+
+#bar chart creation
+def draw():
+    #get the points and the max wins
+    global strategies, MAX_WINS
+
+    #add all data to the chart individually otherwise it will be recognised as one chart
+    for x in range(3):
+        plot.bar(x, strategies[x], label='Strategy ' + str(x + 1))
     
-    #run greedy pig till we have a winner
-    while check():
-        #run greedy pig
-        greedyPig()
+    #set the labels
+    plot.xlabel('Strategy')
+    plot.ylabel('Winning Probability')
 
-#calculate the percentage
-percent = []
-for x in wins:
-    percent.append(int(x/100 * 100))
+    #set the X-axis
+    plot.xticks([0, 1, 2, 3], ['1', '2', '3'])
 
-#plot the data || set evenly spaced data for the X-axis
-xpos = np.arange(len(players))
+    #set the titel
+    plot.title('Pig Game Winning Probabilities')
 
-#set the max for the Y-axis
-plot.axis(ymax=100)
+    #put the small window with the labels
+    plot.legend()
 
-#plot the bars individually
-for x in range(3):
-    plot.bar(x, percent[x], label=players[x] +' (' +str(percent[x]) + '%)', color=['red', 'grey', 'blue'][x])
+    #show the graph
+    plot.show()
 
-#set the X-axis
-plot.xticks(yxos, range(1, 4))
-
-#set labels, title and then show the plot
-plot.xlabel('Strategies')
-plot.ylabel('Win Percentage')
-plot.title('Probability of winning Pig Game')
-
-#show the label for the bars
-plot.legend()
-
-#show the plot
-plot.show()
+#do the research
+research()
+#show the research
+draw()
+print(strategies)
